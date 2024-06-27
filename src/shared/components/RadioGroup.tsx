b1600b48533a6a014b44f94c669f9d265b41f1f8
(주)비디 최윤 <choiyun@bluedigm.com>
2024-06-27 17:43:05 +0900

    feat: radio group btn icon 보완

--- src/shared/components/RadioGroup.tsx
+++ src/shared/components/RadioGroup.tsx
@@ -30,7 +30,7 @@ const RadioGroup = (props) => {
         props.options.map((item, idx) => (
           <div className="m-0 p-0 relative border-[0]">
             <input
-              className="h-auto float-left w-[24px] absolute top-1/4 cursor-pointer"
+              className="h-auto float-left w-[24px] absolute top-1/4 cursor-pointer hidden border-[100px] border-[red]"
               id={`${props.id}_${idx}`}
               name={`${props.id}`}
               type="radio"
@@ -40,7 +40,7 @@ const RadioGroup = (props) => {
               disabled={props.disabled}
             />
             <label
-              className="pl-6 mr-[6px] text-[16px] flex items-center cursor-pointer [word-break:keep-all] before:flex-shrink-0 before:[background-size:100%] label-checked:text-[#333]"
+              className="pl-6 mr-[6px] text-[16px] text-[#333] flex items-center cursor-pointer [word-break:keep-all] before:content-[''] before:block before:flex-shrink-0 before:w-[24px] before:h-[24px] before:mr-[10px] before:bg-[url('./assets/images/btn/btn_icon_radio.png')] before:[background-size:100%]"
               htmlFor={`${props.id}_${idx}`}
             >
               {item.label}
